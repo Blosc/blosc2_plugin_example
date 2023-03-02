4694a188f58eef844aca02087ecf6a731c3bf274
Francesc Alted <francesc@blosc.org>
2023-03-02 17:01:50 +0100

    Build process simplified

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-# from setuptools import setup
-from skbuild import setup
+# Blosc - Blocked Shuffling and Compression Library
+#
+# Copyright (C) 2021  The Blosc Developers <blosc@blosc.org>
+# https://blosc.org
+# License: BSD 3-Clause (see LICENSE.txt)
+#
+# See LICENSE.txt for details about copyright and rights to use.
 
+from skbuild import setup
 
 setup(
     name="plugin_example",
     packages=["plugin_example"],
     package_dir={"plugin_example": "plugin_example"},
 )
-
-
-if __name__ == "__main__":
-    setup()
